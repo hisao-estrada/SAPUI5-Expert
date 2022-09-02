45060089a0eea9aac220ac73f9e1d943237ee127
Hisao Estrada <jhisaoestradap@outlook.com>
2022-09-02 23:05:51 +0000

    Se agrego un nuevo label

--- SAPUI5/webapp/controller/helloPanel.controller.js
+++ SAPUI5/webapp/controller/helloPanel.controller.js
@@ -1,7 +1,7 @@
 //@ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller", 
-    //"sap/m/MessageToast",
+    "sap/m/MessageToast",
     //"sap/ui/model/json/JSONModel"
     //"dtt/SAPUI5/model/Models",
     //"sap/ui/model/resource/ResourceModel"
@@ -12,20 +12,18 @@ sap.ui.define([
      * @param{typeof sap.ui.model.json.JSONModel} JSONModel
      * @param{typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
-    function (Controller ) {
-        return Controller.extend("dtt.SAPUI5.controller.App", {
-
-            
+    function (Controller, MessageToast ) {
+        return Controller.extend("dtt.SAPUI5.controller.helloPanel", {
             onInit: function() {
                 // var i18nModel = new ResourceModel({bundleName:"dtt.SAPUI5.i18n.i18n"})
                 // this.getView().setModel(i18nModel, "i18n")
                 // this.getView().setModel(Models.createRecipient())
+            },
+            onShowHello: function() {
+                var oBundle = this.getView().getModel("i18n").getResourceBundle()
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name")
+                var sMSG = oBundle.getText("helloMsg",[sRecipient])
+                MessageToast.show(sMSG)
             }
-            // onShowHello: function() {
-            //     var oBundle = this.getView().getModel("i18n").getResourceBundle()
-            //     var sRecipient = this.getView().getModel().getProperty("/recipient/name")
-            //     var sMSG = oBundle.getText("helloMsg",[sRecipient])
-            //     MessageToast.show(sMSG)
-            // }
         })
     })
